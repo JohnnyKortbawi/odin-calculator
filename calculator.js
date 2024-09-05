04bc0a764e467687c484118902f7f1e0c3cd59a8
Johnny Kortbawi <anjo2.johnny@gmail.com>
2024-09-05 14:18:25 +0400

    Add basic integer operations functionality

--- calculator.js
+++ calculator.js
@@ -1,8 +1,53 @@
 
-let num1 = 0;
-let num2 = 0;
-let operator = "";
-let result = 0;
+let num1, num2 ,operator, result;
+
+const calculatorDisplay = document.querySelector('#calculator-display');
+clear();
+
+const numericButtons = document.querySelectorAll('.numeric-button');
+numericButtons.forEach((numericButton) => {
+  numericButton.addEventListener('click', () => displayNumber(event.target.innerHTML));
+});
+
+const operatorButtons = document.querySelectorAll('.operator-button');
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => displayOperator(event.target.innerHTML));
+});
+
+const equalButton = document.querySelector('#button-equal');
+equalButton.addEventListener('click', () => {calculate();})
+
+const clearButton = document.querySelector('#button-clear');
+clearButton.addEventListener('click', () => {clear();})
+
+function isNumeric(str) {
+  if (typeof str != "string") return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+function displayNumber(numericCharacter) {
+  calculatorDisplay.textContent += numericCharacter;
+}
+
+function displayOperator(operator) {
+  if(isNumeric(calculatorDisplay.textContent)) {
+    if(calculatorDisplay.textContent)
+    calculatorDisplay.textContent += ` ${operator} `;
+  }
+  else if(calculatorDisplay.textContent) {
+    calculate();
+    calculatorDisplay.textContent += ` ${operator} `;
+  }
+}
+
+function clear() {
+  calculatorDisplay.textContent = '';
+  num1 = 0;
+  num2 = 0;
+  operator = "";
+  result = 0;
+}
 
 function add(num1, num2) {
   return num1 + num2;
@@ -44,4 +89,13 @@ function operate(num1, num2, operator) {
     default: break;
   }
   return result;
+}
+
+function calculate() {
+  let expressions = calculatorDisplay.textContent.split(' ');
+  num1 = parseInt(expressions[0]);
+  num2 = parseInt(expressions[2]);
+  operator = expressions[1];
+  result = operate(num1, num2, operator);
+  calculatorDisplay.textContent = result;
 }
